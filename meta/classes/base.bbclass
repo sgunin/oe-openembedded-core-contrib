sanyaade-embedded-systems/oe-core

oe.packagegroup: add code for package groups (sync from OE)
This includes some utility functions for dealing with groups of packages
defined in the metadata.  Metadata syntax:

    PACKAGE_GROUP_<group> = "<list of packages>"

If the packages in the group are optional:

    PACKAGE_GROUP_<group>[optional] = "1"

Signed-off-by: Chris Larson <chris_larson@mentor.com>

meta/classes/base.bbclass
@@ -9,7 +9,7 @@ inherit utility-tasks
 inherit metadata_scm
 inherit buildstats
 
-OE_IMPORTS += "sys os time oe.path oe.utils oe.data"
+OE_IMPORTS += "sys os time oe.path oe.utils oe.data oe.packagegroup"
 
 def oe_import(d):
     import os, sys

sgunin/oe-openembedded-core-contrib

meta/classes/base.bbclass
@@ -10,7 +10,7 @@ inherit metadata_scm
 inherit buildstats
 inherit logging
 
-OE_IMPORTS += "os sys time oe.path oe.utils oe.data"
+OE_IMPORTS += "os sys time oe.path oe.utils oe.data oe.packagegroup"
 OE_IMPORTS[type] = "list"
 
 def oe_import(d):