4f55ec95f1d3a8055a9a8a7da06fdeee8054337d
Otavio Salvador <otavio@ossystems.com.br>
2022-07-20 16:06:01 -0300

    oeqa: sdk: rust: add basic cargo test
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

---
+++ meta/lib/oeqa/sdk/cases/rust.py
@@ -0,0 +1,31 @@
+#
+# SPDX-License-Identifier: MIT
+#
+
+import os
+import shutil
+import unittest
+
+from oeqa.core.utils.path import remove_safe
+from oeqa.sdk.case import OESDKTestCase
+
+from oeqa.utils.subprocesstweak import errors_have_output
+errors_have_output()
+
+class RustCompileTest(OESDKTestCase):
+    td_vars = ['MACHINE']
+
+    @classmethod
+    def setUpClass(self):
+        shutil.copytree(os.path.join(self.tc.sdk_files_dir, "rust/hello"),
+                        os.path.join(self.tc.sdk_dir, "hello"))
+
+    def setUp(self):
+        machine = self.td.get("MACHINE")
+        if not (self.tc.hasHostPackage("packagegroup-rust-cross-canadian-%s" % machine) or
+                self.tc.hasHostPackage("^rust-cross-canadian-", regex=True) or
+                self.tc.hasHostPackage("^cargo-cross-canadian-", regex=True)):
+            raise unittest.SkipTest("RustCompileTest class: SDK doesn't contain a Rust cross-canadian toolchain")
+
+    def test_cargo_build(self):
+        self._run('cd %s/hello; cargo build' % self.tc.sdk_dir)
